92003fa7915f93af752ad9029f7f1c029fa5bbc9
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2023-12-28 15:48:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-from flask import Flask, jsonify, request, make_response
-from werkzeug.security import generate_password_hash, check_password_hash
-import os
+from flask import Flask
 
 def create_app():
- app = Flask(__name__)
+   app = Flask(__name__)
 
-# Configuración de seguridad
-app.config.update(
- SESSION_COOKIE_SECURE=True,
- SESSION_COOKIE_HTTPONLY=True,
- SESSION_COOKIE_SAMESITE='Lax',
-)
+   # Configuración de seguridad
+   app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+   )
 
-# Ruta de registro
-@app.route('/register', methods=['GET', 'POST'])
-def signup_user():
- pass
- # Tu código para registrar al usuario va aquí
+   # Ruta de registro
+   @app.route('/register', methods=['GET', 'POST'])
+   def signup_user(): 
+    # Tu código para registrar al usuario va aquí
 
-# Ruta de login
-@app.route('/login', methods=['GET', 'POST']) 
-def login_user():
- pass
- # Tu código para iniciar sesión va aquí
+   # Ruta de login
+   @app.route('/login', methods=['GET', 'POST']) 
+   def login_user(): 
+    # Tu código para iniciar sesión va aquí
 
-if __name__ == "__main__":
-  app.run(debug=True)
+   return app
+
 
