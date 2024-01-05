edbc58e05dd38a7ef8c152ec1ebe178cd534f3eb
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-01-05 13:52:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from wtforms.validators import DataRequired
 from flask_wtf.csrf import CSRFProtect
 from flask_talisman import Talisman
 import os
-import logging
-
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 class MyForm(FlaskForm):
@@ -26,38 +23,28 @@ def create_app():
    SESSION_COOKIE_SAMESITE='Lax',
  )
 
- @app.before_request
- def log_request_info():
-   app.logger.info('Headers: %s', request.headers)
-   app.logger.info('Body: %s', request.get_data())
 
  @app.route("/", methods=['GET', 'POST'])
  def index():
-   logging.info('Index page accessed')
    form = MyForm()
    if form.validate_on_submit():
-       logging.info('Form submitted successfully')
        return 'Success!'
    return render_template('index.html', form=form)
 
  @app.route("/")
  def hello():
-  logging.info('Hello page accessed')
   return render_template('index.html')
 
  @app.route('/', methods=['GET'])
  def home():
-   logging.info('Home page accessed')
    return 'Hello world'
 
  @app.route('/register', methods=['GET', 'POST'])
  def signup_user():
-   logging.info('Register page accessed')
    return 'Registration page'
 
  @app.route('/login', methods=['GET', 'POST']) 
  def login_user():
-   logging.info('Login page accessed')
    return 'Login page'
 
  @app.after_request
@@ -68,10 +55,6 @@ def create_app():
    response.headers["Nonce"] = nonce
    response.headers['Server'] = ''
    return response
-
- # Add StreamHandler to the application's logger
- stream_handler = logging.StreamHandler()
- app.logger.addHandler(stream_handler)
 
  return app
 
