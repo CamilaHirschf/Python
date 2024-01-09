a8afb5649aff221648f5590921b27b6e9a266ef9
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-01-09 14:24:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,13 @@ users = [
 ]
 
 class User(UserMixin):
- def __init__(self, id, username, password):
-     self.id = id
-     self.username = username
-     self.password = password
+   def __init__(self, id, username, password):
+       self.id = id
+       self.username = username
+       self.password = password
+
+   def is_active(self):
+       return True
 
 class LoginForm(FlaskForm):
   username = StringField('Username')
@@ -64,10 +67,13 @@ def create_app():
   return render_template('index.html', form=form)
 
  @login_manager.user_loader
- def load_user(user_id):
-  # Busca al usuario en la lista de usuarios
-  user = next((u for u in users if u['id'] == int(user_id)), None)
-  return user
+def load_user(user_id):
+   # Busca al usuario en la lista de usuarios
+   user_data = next((u for u in users if u['id'] == int(user_id)), None)
+   if user_data is not None:
+       return User(user_data['id'], user_data['username'], user_data['password'])
+   else:
+       return None
  
  @app.route('/register', methods=['GET', 'POST'])
  def signup_user():
