ff0c75bb438e59de77db26b0689b6e81d7669f40
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-01-09 15:42:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, url_for
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
