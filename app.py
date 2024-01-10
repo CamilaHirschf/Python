55a54687f42d53a46c8c677a138190b497861a38
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-01-10 16:16:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
