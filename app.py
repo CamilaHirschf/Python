edcd45b372e7e9416e6cffb5e470b29e483ca52e
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-01-10 13:15:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,8 @@ def create_app():
   form = LoginForm()
   if form.validate_on_submit():
   # Busca al usuario en la lista de usuarios
-   user = next((u for u in users if u.username == form.username.data), None)
-   if user and check_password_hash(user.password, form.password.data):
+   user = next((u for u in users if u['username'] == form.username.data), None)
+   if user and check_password_hash(user['password'], form.password.data):
     login_user(user)
     return redirect(url_for('dashboard'))
   return render_template('login.html', form=form)
