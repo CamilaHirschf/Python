a9bb26d0afda83005e5426c490fde9022fcede86
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-01-10 14:22:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,15 +92,15 @@ def create_app():
 
 
  @app.route('/login', methods=['GET', 'POST'])
-def login():
- form = LoginForm()
- if form.validate_on_submit():
-  # Busca al usuario en la lista de usuarios
-  user = next((u for u in users if u.username == form.username.data), None)
-  if user and check_password_hash(user.password, form.password.data):
-     login_user(user)
-     return redirect(url_for('dashboard'))
- return render_template('login.html', form=form)
+ def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+   # Busca al usuario en la lista de usuarios
+   user = next((u for u in users if u.username == form.username.data), None)
+   if user and check_password_hash(user.password, form.password.data):
+      login_user(user)
+      return redirect(url_for('dashboard'))
+  return render_template('login.html', form=form)
 
 
  @app.route('/dashboard')
