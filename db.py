8357863a4f6f4ab1aea091e9743bd9d479514c47
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-01-11 11:55:51 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+   id = db.Column(db.Integer, primary_key=True)
+   username = db.Column(db.String(30), unique=True, nullable=False)
+   password_hash = db.Column(db.String(128))
+
+   def set_password(self, password):
+       self.password_hash = generate_password_hash(password)
+
+   def check_password(self, password):
+       return check_password_hash(self.password_hash, password)
