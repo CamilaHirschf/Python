a3c3777d0ce5fbae459821a12e912c1665135a55
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2024-01-11 12:17:11 -0300

    Update db.py

--- db.py
+++ db.py
@@ -4,13 +4,13 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
-   __table_args__ = {'extend_existing': True}
-   id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String(30), unique=True, nullable=False)
-   password_hash = db.Column(db.String(128))
+ __table_args__ = {'extend_existing': True}
+ id = db.Column(db.Integer, primary_key=True)
+ username = db.Column(db.String(30), unique=True, nullable=False)
+ password_hash = db.Column(db.String(128))
 
-   def set_password(self, password):
-       self.password_hash = generate_password_hash(password)
+ def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
 
-   def check_password(self, password):
-       return check_password_hash(self.password_hash, password)
+ def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
